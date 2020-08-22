c803fa68d9f175a8416e5d88f48692f8e67122e8
ayumi kamada <dmrdmr58@gmail.com>
2020-08-22 23:26:25 +0900

    Initial Commit

---
+++ db/schema.rb
@@ -0,0 +1,23 @@
+ActiveRecord::Schema.define(version: 2020_08_21_131405) do
+
+  enable_extension "plpgsql"
+
+  create_table "pictures", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pictures_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  add_foreign_key "pictures", "users"
+end
