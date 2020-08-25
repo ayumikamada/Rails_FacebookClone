34367a19787a0ad2f7e2662908a4755e58dc53c4
ayumi kamada <dmrdmr58@gmail.com>
2020-08-25 23:06:47 +0900

    インデント等調整

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,11 +1,9 @@
 class PicturesController < ApplicationController
-
   before_action :set_picture, only: [:show, :edit, :update, :destroy]
 
   def index
     @pictures = Picture.all.order(created_at: :desc)
   end
-
   def new
     if params[:back]
       @picture = Picture.new(picture_params)
@@ -13,13 +11,11 @@ class PicturesController < ApplicationController
       @picture = Picture.new
     end
   end
-
   def confirm
     @picture = Picture.new(picture_params)
     @picture.user_id = current_user.id
     render :new if @picture.invalid?
   end
-
   def create
     @picture = Picture.new(picture_params)
     @picture.user_id = current_user.id
@@ -33,14 +29,11 @@ class PicturesController < ApplicationController
       end
     end
   end
-
   def show
     @user = User.find_by(id: @picture.user_id)
   end
-
   def edit
   end
-
   def update
     if @picture.update(picture_params)
       redirect_to pictures_path, notice: "編集完了！"
@@ -48,18 +41,15 @@ class PicturesController < ApplicationController
       render :edit
     end
   end
-
   def destroy
     @picture.destroy
     redirect_to pictures_path, notice:"削除完了！"
   end
 
   private
-
   def picture_params
     params.require(:picture).permit(:title, :content, :image, :image_cache)
   end
-
   def set_picture
     @picture = Picture.find(params[:id])
   end
